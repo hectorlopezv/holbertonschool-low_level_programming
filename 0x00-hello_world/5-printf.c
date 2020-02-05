10cd2f1a061d0cec28aeb48219918f9f726f3d11
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-05 03:39:04 +0000

    hector

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Sucess)
+  */
+int main(void)
+{
+char str2[] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n", str2);
+return (0);
+}
