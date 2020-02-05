3e1be0baf42e66b1f90e0a8c5c41e667aac9deba
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-05 17:04:05 +0000

    mandatory

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = '0'; i <= '9'; i++)
+{
+for (j = '0'; j <= '9'; j++)
+{
+putchar(i);
+putchar(j);
+if (i == '9' && j == '9')
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int b;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+for (b = 'a'; b <= 'f'; b++)
+{
+putchar(i);
+}
+putchar(' ');
+return (0);
+}
