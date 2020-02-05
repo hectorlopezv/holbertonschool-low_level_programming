597f7d3d88b5b85f5149036d0d4f75f2f4b3eff1
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-05 02:48:33 +0000

    hector

--- 0x00-hello_world/4-put.c
+++ 0x00-hello_world/4-put.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
-int main()
+/*
+ * This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines
+ */
+
+int main(void)
 {	char str[]
-	str="Programming is like building a multilingual puzzle
+	str = "Programming is like building a multilingual puzzle
 	put(str)
 	return(0)
 }
