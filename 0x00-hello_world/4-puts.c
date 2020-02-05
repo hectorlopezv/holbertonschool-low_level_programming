23d2c6908af3f400a5647b3218609014e9a36c00
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-05 03:28:05 +0000

    ejemplo2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-/* Use this */
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Sucess)
+  */
 int main(void)
 {/*hello2*/
 char str[] = "\"Programming is like building a multilingual puzzle";
