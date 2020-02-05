21a258a32a3c9e13f7bd372c09ad1214ef1372b6
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-05 03:22:11 +0000

    hector lopez2

--- 0x00-hello_world/4-put.c
+++ 0x00-hello_world/4-put.c
@@ -1,16 +1,8 @@
 #include<stdio.h>
-/*
- * This is the preferred style for multi-line
- * comments in C source code.
- * Please use it consistently.
- *
- * Description:  A column of asterisks on the left side,
- * with beginning and ending almost-blank lines
- */
-
+/* Use this */
 int main(void)
-{	char str[]
-	str = "Programming is like building a multilingual puzzle
-	put(str)
-	return(0)
+{/*hello2*/
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
 }
