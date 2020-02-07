8c12075f8b9f0c9cdb144b08649aeff913e7adef
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-07 22:43:23 +0000

    hector

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+
+for (i = 0 ; i <= 99 ; i++)
+{
+for (j = i + 1 ; j <= 99 ; j++)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i == 'b' && j == 'c')
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
