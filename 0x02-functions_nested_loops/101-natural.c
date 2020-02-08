81008b41a7485612c8f664cf786c3031016bc16f
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-08 23:54:33 +0000

    hectorrr

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - prints the sum of all multiples
  * of 3 or 5 below 1024
@@ -8,23 +9,18 @@
 
 int main(void)
 {
-int i_3;
-i_3 = 340 + 1;
-int sum_3;
-sum_3 = 0;
-int b_5;
-b_5 = 204;
-int sum_5;
-sum_5 = 0;
-int c_15;
-c_15 = 68;
-int res_15;
-res_15 = 15;
-int total;
-sum_3 = 3 * 0.5 * i_3 * (i_3 + 1);
-sum_5 = 5 * 0.5 * b_5 * (b_5 + 1);
-res_15 = 15 * 0.5 * c_15 * (c_15 + 1);
-total = sum_5 + sum_3 - res_15;
-printf("%d\n", total);
-return (0);
+	int i, a, b, sum;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		a = i % 3;
+		b = i % 5;
+		if (a == 0 || b == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
