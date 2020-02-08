bc6193bb5dccb6d1195c662b991716b8deee56ad
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-08 22:34:58 +0000

    hectorrr

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_islower - prints if a number is positive
+ *
+ *Return: 1 if is the alphabet
+ * on error return 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
