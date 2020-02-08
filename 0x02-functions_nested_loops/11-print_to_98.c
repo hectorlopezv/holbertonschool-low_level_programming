681937570f9dd92ad4885303a05bbe279fcdc367
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-08 23:37:46 +0000

    hectorrr

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,48 @@
 #include "holberton.h"
+#include "stdio.h"
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int main(void)
+void print_to_98(int n)
 {
-print_to_98(0);
-print_to_98(98);
-print_to_98(111);
-print_to_98(81);
-print_to_98(-10);
-return (0);
+int c;
+c = (n > 98) ? 1 : 0;
+if (c)
+{
+while (n >= 98)
+{
+printf("%d", n);
+if (n == 98)
+
+{
+printf("\n");
+n--;
+continue;
+}
+printf("%c", ',');
+printf("%c", ' ');
+n--;
+}
+
+}
+else
+{
+while (n <= 98)
+{
+printf("%d", n);
+if (n == 98)
+{
+printf("\n");
+n++;
+continue;
+}
+printf("%c", ',');
+printf("%c", ' ');
+n++;
+}
+
+
+}
 }
