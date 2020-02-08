772837acf296a3e36731ce738d99ca8825a74fee
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-08 23:15:27 +0000

    hectorrr

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,32 @@
 #include "holberton.h"
-
 /**
- * main - check the code for Holberton School students.
+ * _jack_bauer - writes the character c to stdout
  *
- * Return: Always 0.
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+void jack_bauer(void)
 {
-jack_bauer();
-return (0);
+int i;
+int j;
+i = 0;
+j = 0;
+while (j <= 23)
+{
+while (i <= 59)
+{
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+_putchar(':');
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar('\n');
+i++;
+}
+
+i = 0;
+j++;
+
+}
 }
