3c0d92860ba4a4653b7f0cb4c896e82be14bde91
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-08 22:17:30 +0000

    hectorrr

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,17 @@
 #include "holberton.h"
+/**
+ *print_alphabetx10 - uses the print_alphabet to do it 10 times
+ *
+ *Return: void
+ */
 
-int main(void)
+void print_alphabet_x10(void)
 {
-print_alphabet_x10();
-return (0);
+int i;
+
+for (i = 0; i <= 10; i++)
+{
+print_alphabet();
+}
+
 }
