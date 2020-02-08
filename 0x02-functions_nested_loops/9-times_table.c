29ef3e1dd282598547201255d2ee45fc30435ef8
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-08 23:26:41 +0000

    hectorrr

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,47 @@
 #include "holberton.h"
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * times-.variable - writes the character c to stdout
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+void times_table(void)
 {
-times_table();
-return (0);
+int i;
+int n;
+int p;
+n = 0;
+i = 0;
+p = 0;
+while (i <= 9)
+{
+while (n <= 9)
+{
+p = i *n;
+if (p > 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(p / 10 + '0');
+_putchar(p % 10 + '0');
+
+}
+else
+{
+if (n == 0)
+{
+_putchar(p + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(p + '0');
+}
+}
+n++;
+}
+_putchar('\n');
+n = 0;
+i++;
+}
 }
