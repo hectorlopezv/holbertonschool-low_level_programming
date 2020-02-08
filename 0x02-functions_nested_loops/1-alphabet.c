8e69ef7ce8a54f926072fd00ca508c53dc0b0867
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-08 22:04:07 +0000

    hectorrr

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
+#include <stdio.h>
+
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
+/**
+ *print
+ *
+ *Return: nothing
+ */
 int i;
 for (i = 'a'; i <= 'z'; i++)
 {
