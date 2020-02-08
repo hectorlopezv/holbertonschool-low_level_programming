ad7d6198491fed2e73a04d9b5326d2e045e78ac0
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-08 21:55:29 +0000

    hectorrr

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,15 +1,12 @@
 #include "holberton.h"
+
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
-	int i;
-	for(i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+    print_alphabet();
+    return (0);
 }
