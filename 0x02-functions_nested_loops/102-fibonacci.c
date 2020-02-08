d7f68ee80e5a8f114d26e6995ec5e29dd69d2db6
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-08 19:57:44 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+int main(void){
+long int n_0=1;
+long int n_1=2;
+long int nextTerm;
+for (int f=1;f<50;f++){
+if (f==49)
+{
+printf("%ld\n",n_0);
+}
+else
+{
+printf("%ld, ",n_0);
+}
+nextTerm = n_0 +n_1;
+n_0 =n_1;
+n_1=nextTerm;
+}
+return (0);
+
+}
