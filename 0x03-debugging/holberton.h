8eb608d3bf79835e2302027d072bb22c7123b92c
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-09 06:47:45 +0000

    hector

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,8 +1,17 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 #include <stdio.h>
 
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+
 #endif /* HOLBERTON_H */
