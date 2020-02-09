da1a8fd34221340abd9af0daca547d7bb05eca92
Hector vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-02-09 20:36:20 +0000

    hector

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,48 +2,47 @@
 #include "holberton.h"
 
 /**
- * print_fizz - this is the description os
+ * main - this is the description os
  *
  * Return: Always 1 if uppercase 0 otherwise
  */
-void print_fizz(void)
-{
-	int i;
 
+int main(void)
+{
+	print_fizz();
+	return (0);
+}
+
+/**
+ * print_fizz - this is
+ *
+ *Return: always
+ */
+void print_fizz(void)
+{int i;
 	for (i = 1; i <= 100; i++)
 	{
-
 		if (i % 5 == 0 && i % 3 == 0)
 		{
 			printf("FizzBuzz ");
-
 		} else if (i % 5 == 0)
+
 		{
-			if (i ==100)
-
+			if (i == 100)
 			{
-
-
 				printf("Buzz");
 				printf("\n");
 			} else
 			{
 				printf("Buzz ");
 			}
-
-
 		} else if (i % 3 == 0)
 		{
 			printf("Fizz ");
-
-
 		} else
-
 		{
 			printf("%d ", i);
-
-
 		}
 	}
-
 }
+
