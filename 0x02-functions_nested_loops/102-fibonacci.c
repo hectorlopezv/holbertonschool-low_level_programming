1a5336faef355d0fadb3b19fa18ded664566f6f3
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-09 01:09:03 +0000

    bebe

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-<<<<<<< HEAD
-=======
 /**
  * main  - fibonacci n=50
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
->>>>>>> 0781df2a42109fe7cfcf1901cf45e0f39a01517a
 int main(void)
 {
-long  n_0=1;
-long  n_1=2;
-long  nextTerm;
+long n_0 = 1;
+long n_1 = 2;
+long nextTerm;
 int f;
 for (f = 1; f <= 50; f++)
 {
