a140d50a30a8724c2ff66564b08015eb0186f260
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-09 13:06:17 +0000

    hecto2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,7 @@
 #include "holberton.h"
-
 /**
  *print_times_table - print fibo
  *@n: number of times
- *Return: 0
  */
 
 void print_times_table(int n)
@@ -14,33 +12,28 @@ if (n <= 15 && n >= 0)
 for (row = 0; row <= n; row++)
 {
 for (col = 0; col <= n; col++)
-{
-product = row * col;
+{product = row * col;
 units = (product % 10);
 tens = (product / 10);
 if (product > 9 && col <= n && product <= 99)
-{
-_putchar(',');
+{_putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(tens + '0');
 _putchar(units + '0');
 }
 else if (product == 0 && col == 0)
-{
-_putchar(units + '0');
+{_putchar(units + '0');
 }
 else if (product <= 9 && col >= 1)
-{
-_putchar(',');
+{_putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(units + '0');
 }
 else if (product > 99 && col <= n)
-{
-_putchar(',');
+{_putchar(',');
 _putchar(' ');
 units = (product % 10);
 tens = product / 10 % 10;
@@ -48,11 +41,8 @@ hund = (product / 100);
 _putchar(hund + '0');
 _putchar(tens + '0');
 _putchar(units + '0');
-}
-}
+}}
 _putchar('\n');
-}
-}
-}
+}}}
 
 
