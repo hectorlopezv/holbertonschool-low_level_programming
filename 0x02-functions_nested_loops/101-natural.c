479bce1bc3347be17c82b0d971fa73b8ab5f61e0
Hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-02-09 00:54:36 +0000

    hectorrrrrrr

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,16 +8,23 @@
  */
 int main(void)
 {
-int i_3, b_5, c_15, sum_3, sum_5, res_15, total;
-i_3 = 341;
-b_5 = 204;
-c_15 = 68;
-res_15 = 15;
-sum_3 = 3 * 0.5 * i_3 * 342;
-sum_5 = 5 * 0.5 * b_5 * 205;
-res_15 = 15 * 0.5 * c_15 * 69;
-total = sum_5 + sum_3 - res_15;
-printf("%d\n", total);
+int a_3, b_5, c, p;
+c = 0;
+for (p = 0; p <= 1023; p++)
+{
+a_3 = p % 3;
+b_5 = p % 5;
+if (a_3 == 0 || b_5 == 0)
+{
+c += p;
+if (a_3 == 3 && b_5 == 0)
+{
+c -= p;
+}
+
+}
+}
+printf("%d\n", c);
 return (0);
 }
 
