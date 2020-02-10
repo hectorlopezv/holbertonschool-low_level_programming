457570e2985838b6e17969bbcffb443e8909ba80
Hector vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-02-10 02:50:40 +0000

    hector

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,3 @@
-#include<stdio.h>
-#include<math.h>
 #include "holberton.h"
 /**
  * print_number - check the code for Holberton School students.
@@ -7,18 +5,22 @@
  * Return: Always 0.
  */
 
+int sacar_digitos(int n);
+int sacar_divisor(int n);
+
 void print_number(int n)
 {
-	int i, h, o, d;
+	unsigned int i, h, o, d;
 
 
 	if (n > 0)
 	{
-		i = (int)(log10(n) + 1);
+
+		i = sacar_digitos(n);
 		o = i - 1;
 		for (h = 0; h < i; h++)
 		{
-			d = pow(10, o);
+			d = sacar_divisor(o);
 			_putchar(n / d + '0');
 			n = n % d;
 			o--;
@@ -29,18 +31,56 @@ void print_number(int n)
 	{
 		_putchar(45);
 		n *= -1;
-		i = (int)(log10(n) + 1);
+		i = sacar_digitos(n);
 		o = i - 1;
 		for (h = 0; h < i; h++)
 		{
-			d = pow(10, o);
+			d = sacar_divisor(o);
 			_putchar(n / d + '0');
 			n = n % d;
 			o--;
 		}
 
-	} else if( n == 0)
+	} else if (n == 0)
 	{
 		_putchar('0');
 	}
 }
+
+/**
+ * sacar_digitos - check the code for Holberton School students.
+ *@n: hello
+ * Return: Always 0.
+ */
+int sacar_digitos(int n)
+{
+	int count = 0;
+
+	while (n > 0)
+	{
+		n = n / 10;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * sacar_divisor - check the code for Holberton School students.
+ *@n: hello
+ * Return: Always 0.
+ */
+int sacar_divisor(int n)
+{
+	int divisor, pe;
+
+	divisor = 1;
+	pe = n;
+	for (; pe > 0; pe--)
+	{
+		divisor *= 10;
+	}
+
+	return (divisor);
+}
+
