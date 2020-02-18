dcb126b1369a27cb1f661c82f1c632d548cef55b
hector lopez <hectorvmlopez@gmail.com>
2020-02-18 15:03:05 +0000

    hector

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -23,7 +23,7 @@ void puts_half(char *str)
 
 
 
-	for(;largo<lenght;largo++)
+	for(;largo<=lenght;largo++)
 	{
 		_putchar(str[largo]);
 
@@ -52,6 +52,6 @@ int _strlen(char *s)
 		i++;/* moving the pointer one index*/
 		count++;
 	}
-	count--;
+
 	return (count);
 }
