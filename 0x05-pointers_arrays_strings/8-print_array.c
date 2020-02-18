1a11683bcb0c2d9ebf2d00e74f5869e64c171fc1
hector lopez <hectorvmlopez@gmail.com>
2020-02-18 05:21:52 +0000

    8 exercise

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - check the code for Holberton School students.
+ *@a: integer array
+ *@n:  size integer value
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	for (int i = 0; i < n; i++)
+
+	{
+		if (i == n - 1)
+		{
+			printf("%d", a[i]);
+
+		} else
+		{
+			printf("%d, ", a[i]);
+		}
+
+	}
+	printf("\n");
+
+}
