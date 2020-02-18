cb1cf5cf9c668feb1ad3f822647dd065868d7361
hector lopez <hectorvmlopez@gmail.com>
2020-02-18 19:00:39 +0000

    hector lopez

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,30 +1,35 @@
 #include "holberton.h"
 #include <stddef.h>
+
 /**
- *  * puts_half - check the code for Holberton School students.
- *   *@s: string variable
- *    * Return: Always 0.
- *     */
+ * puts_half - check the code for Holberton School students.
+ *@s: string variable
+ * Return: Always 0.
+ */
 
 void puts_half(char *s)
 {	/*Guard conditions*/
-	if(s[0] == '\0' || s == NULL)
+	if (s[0] == '\0' || s == NULL)
 	{
-
-
 		return;
 	}
-
 	else
 	{
 
 		int c, a;
+
 		for (a = 0; s[a] != '\0'; a++)
 		{
 			/*CONTADOR*/
 		}
+		if (a == 1)
+		{
+			_putchar(s[0]);
+			_putchar('\n');
+			return;
+		}
 
-		c = (a %2 == 0) ? a/2 : (a+1)/2;
+		c = (a % 2 == 0) ? a / 2 : (a + 1) / 2;
 
 		for (; s[c] != '\0'; c++)
 		{
