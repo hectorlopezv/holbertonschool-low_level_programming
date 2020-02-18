f262508653dcf4b354934fbd5f2d075d221f89a5
hector lopez <hectorvmlopez@gmail.com>
2020-02-18 18:48:53 +0000

    hector

--- 0x05-pointers_arrays_strings/7-main.c
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	char *str;
-	str = "01234567898";
+	str = "0123456789";
 
 
 	puts_half(str);
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,62 +1,38 @@
 #include "holberton.h"
 #include <stddef.h>
 /**
- * puts_half - check the code for Holberton School students.
- *@s: string variable
- * Return: Always 0.
- */
+ *  * puts_half - check the code for Holberton School students.
+ *   *@s: string variable
+ *    * Return: Always 0.
+ *     */
 
 void puts_half(char *s)
-{
-
-
-	int lenght, c;
-
-	lenght = _strlen(s);
-	c = (lenght % 2 == 0) ? lenght / 2 : (lenght - 1) / 2;
-
-
-	for (; s[c] != '\0'; c++)
+{	/*Guard conditions*/
+	if(s[0] == '\0' || s == NULL)
 	{
-		_putchar(s[c]);
+
+
+		return;
 	}
 
-	/*
-	 * for(;largo<=lenght;largo++)
-	 * {
-	 * _putchar(str[largo]);
-	 *
-	 * }
-	 */
-
-	/*
-	 * while (s[i] != '\0')
-	 * {
-	 * counter++;
-	 * i++;
-	 *  }
-	 */
-	_putchar('\n');
-}
-
-/**
- * _strlen - check the code for Holberton School students.
- *@s: string variable
- * Return: Always 0.
- */
-
-int _strlen(char *s)
-{
-	int count, i;
-
-	count = 0;
-	i = 0;
-
-	while (s[i] != '\0')
+	else
 	{
-		i++;/* moving the pointer one index*/
-		count++;
+
+		int c, a;
+		for (a = 0; s[a] != '\0'; a++)
+		{
+			/*CONTADOR*/
+		}
+
+		c = (a %2 == 0) ? a/2 : (a+1)/2;
+
+		for (; s[c] != '\0'; c++)
+		{
+			_putchar(s[c]);
+		}
+		_putchar('\n');
+
 	}
 
-	return (count);
 }
+
