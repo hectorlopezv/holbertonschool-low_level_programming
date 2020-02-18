40436a8883fd42495ac11f137885368d3aaecb83
hector lopez <hectorvmlopez@gmail.com>
2020-02-18 04:58:47 +0000

    exercise 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * puts_half - check the code for Holberton School students.
+ *@str: string variable
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int lenght, i;
+
+	lenght = _strlen(str);
+
+	if (lenght % 2 ==  0)/* even*/
+	{
+		lenght = lenght / 2;
+	} else if (lenght % 2 != 0)/* odd*/
+	{
+		lenght = (lenght - 1) / 2;
+	}
+	str += lenght;
+	for (i = lenght; i < lenght * 2; i++, str++)
+	{
+
+		_putchar(*str);
+
+
+	}
+	_putchar('\n');
+}
+
+int _strlen(char *s)
+{
+
+	int count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		s++;/* moving the pointer one index*/
+		count++;
+	}
+	return (count);
+}
