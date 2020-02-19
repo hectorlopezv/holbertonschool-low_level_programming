f68768366e16d1e042e846378ee14a802d452f4f
hector lopez <hectorvmlopez@gmail.com>
2020-02-19 13:36:12 +0000

    hector2

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,106 @@
+#include "holberton.h"
+/**
+ *  * print_number - check the code for Holberton School students.
+ *   *@n: hello
+ *    * Return: Always 0.
+ *     */
+
+int sacar_digitos(int n);
+int sacar_divisor(int n);
+
+void print_number(int n)
+{
+	int i, h, o, d;
+
+	if (n > 0)
+	{
+
+		i = sacar_digitos(n);
+		o = i - 1;
+		for (h = 0; h < i; h++)
+		{
+			d = sacar_divisor(o);
+			_putchar(n / d + '0');
+			n = n % d;
+			o--;
+		}
+
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		i = sacar_digitos(n);
+		o = i - 1;
+		for (h = 0; h < i; h++)
+		{
+			d = sacar_divisor(o);
+			_putchar(((n / d) * -1) + '0');
+			n = n % d;
+			o--;
+		}
+
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+}
+
+
+/**
+ *  * sacar_digitos - check the code for Holberton School students.
+ *   *@n: hello
+ *    * Return: Always 0.
+ *     */
+int sacar_digitos(int n)
+{
+	int count = 0;
+
+	if (n < 0)
+
+	{
+		while (n < 0)
+		{
+			n = n / 10;
+			count++;
+		}
+	} else
+
+	{
+
+		while (n > 0)
+		{
+			n = n / 10;
+			count++;
+		}
+
+	}
+
+
+	return (count);
+}
+
+/**
+ *  * sacar_divisor - check the code for Holberton School students.
+ *   *@n: hello
+ *    * Return: Always 0.
+ *     */
+int sacar_divisor(int n)
+{
+	int divisor;
+
+	if (n < 0)
+	{
+		n = n * -1;
+
+	}
+
+	divisor = 1;
+
+	for (; n != 0; n--)
+	{
+		divisor *= 10;
+	}
+
+	return (divisor);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -10,3 +10,4 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 int _strlen(char *s);
+int _putchar(char c);
