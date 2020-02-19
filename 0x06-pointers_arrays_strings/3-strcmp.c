2c1774f761c86d8c6a8372b22b40dc492dff1742
hector lopez <hectorvmlopez@gmail.com>
2020-02-19 14:29:51 +0000

    hector

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,40 +6,40 @@
  *@s2: second string
  *Return: a number if certain condition
  */
-
 int _strcmp(char *s1, char *s2)
 
 {
-	int sum_s1, sum_s2;
-	int i, j, condition;
 
-	sum_s1 = 0;
-	sum_s2 = 0;
-	condition = 0;
-	/* uppercase sum 2*/
-	/* lowercase sum 1*/
+	/**
+	 *     * Here we have two strings str1 = "jkl" and str2 = "jkq".
+	 * Comparison starts off by comparing the first character from str1 and
+	 * str2 i.e 'j' from "jkl" and 'j' from "jkm", as they are equal, the next
+	 * two characters are compared i.e 'k' from "jkl" and 'k' from "jkm", as
+	 * they are also equal, again the next two characters are compared i.e 'l'
+	 * from "jkl" and 'q' from "jkm", as ASCII value of 'q' (113) is greater
+	 * than that of 'l' (108),
+	 * Therefore str2 is greater than str1 and strcmp() will return 5
+	 * ( i.e 113-108 = 5 ) .
+	 */
 
-	/* loop for s1*/
+	int i, resta;
+
+	resta = 0;
+
 	for (i = 0; s1[i] != '\0'; i++)
 	{
-		sum_s1 += s1[i];
-
+		if (s1[i] < s2[i])
+		{
+			resta = s1[i] - s2[i];
+			return (resta);
+		} else if (s1[i] > s2[i])
+		{
+			resta = s1[i] - s2[i];
+			return (resta);
+		}
 	}
 
-	/* loop for s2*/
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		sum_s2 += s2[j];
+	return (resta);
 
-	}
-
-	condition = s1 - s2;
-
-
-	if (s2 == s1)
-	{
-		condition = 0;
-	}
-
-	return (condition);
 }
+
