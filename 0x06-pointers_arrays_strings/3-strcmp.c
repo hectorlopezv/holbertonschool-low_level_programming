a8efdaae6ed954064569ade967f9bd1ba660a530
hector lopez <hectorvmlopez@gmail.com>
2020-02-19 12:33:40 +0000

    hector

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+
+/**
+ *_strcmp - compare strings
+ *@s1: first string
+ *@s2: second string
+ *Return: a number if certain condition
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+	int sum_s1, sum_s2;
+	int i, j, condition;
+
+	sum_s1 = 0;
+	sum_s2 = 0;
+	/* uppercase sum 2*/
+	/* lowercase sum 1*/
+
+	/* loop for s1*/
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		sum_s1 = sum_s1 + s1[i];
+
+	}
+
+	/* loop for s2*/
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		sum_s2 = sum_s2 + s2[j];
+
+	}
+
+	if (s1 < s2)
+	{
+
+		condition = -15;
+	}
+
+	if (s2 < s1)
+	{
+		condition = 15;
+	}
+
+	if (s2 == s1)
+	{
+		condition = 0;
+	}
+
+	return (condition);
+}
