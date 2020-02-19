950fa0a555c46a6171d5582174651bbc97b9ec6c
hector lopez <hectorvmlopez@gmail.com>
2020-02-19 02:34:00 +0000

    hector

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,14 +12,14 @@ int _atoi(char *s)
 	unsigned int str, y, i;
 
 	str = 0;
-	y=0;
+	y = 0;
 	i = 0;
-	n =1;
-	contador= 0;
+	n = 1;
+	contador = 0;
 
 	for (i = 0 ; s[i] != '\0'; i++)
 	{
-		if ((contador > 0) &( s[i] == ' ') )
+		if ((contador > 0) && (s[i] == ' '))
 		{ /* break space*/
 			str = str;
 			break;
@@ -37,7 +37,7 @@ int _atoi(char *s)
 			contador++;
 			y = s[i] - '0';
 
-			str = (str*10) + y;
+			str = (str * 10) + y;
 		}
 	}
 	return (str * n);
