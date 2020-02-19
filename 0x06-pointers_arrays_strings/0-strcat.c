1811f8946d9c96a2765be30896b80bf5ac0ebbea
hector lopez <hectorvmlopez@gmail.com>
2020-02-19 09:17:55 +0000

    hector

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenate strings
+ *@desitny: desitny string
+ *@source: source string
+ *Return: a string == pointer
+ */
+
+char *_strcat(char *desitny, char *source)
+{
+	/* counts desitny legnth counting '\0*/
+	int size, size_source;
+
+	for (size = 0; desitny[size] != '\0'; size++)
+	{
+		/* count size of dest including null*/
+	}
+
+	for (size_source = 0; source[size_source] != '\0'; size_source++)
+	{
+		desitny[size + size_source] = source[size_source];
+	}
+
+	source[size_source + size] = '\0';/* add null character*/
+	return (desitny);
+
+}
