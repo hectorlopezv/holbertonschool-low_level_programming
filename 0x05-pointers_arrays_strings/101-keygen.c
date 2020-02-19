dc51b4ebd3bb6121336366c3ae111fe2626b4e59
hector lopez <hectorvmlopez@gmail.com>
2020-02-19 03:46:27 +0000

    hector

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,6 +2,12 @@
 #include <time.h>
 #include <stdlib.h>
 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 
 {
@@ -10,27 +16,27 @@ int main(void)
 	srand(time(0));
 	/*random number between a range*/
 	/*num = (rand() % (62-0 + 1)) + 0;*/
-	
+
 	int sum, i, r;
-	char a[63] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	char a[63] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345678";
 	char password[100];
 
-	
+
 	sum = 0;
 	i = 0;
 
 	while (sum < (2772 - 122))
 	{
-		r = (rand() % (62-0 + 1)) + 0;
+		r = (rand() % (62 - 0 + 1)) + 0;
 		password[i] = a[r];
 		sum += password[i];
 		i++;
 	}
+
 	r = 2772 - sum;
 	password[i] = r;
 	printf("%s\n",  password);
-	
-
 
 	return (0);
 }
