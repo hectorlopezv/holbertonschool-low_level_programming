e71786a536b80dff1b0f3204227ba820dc68b050
hector lopez <hectorvmlopez@gmail.com>
2020-02-21 12:44:28 +0000

    9 exercise

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *set_string - Sets the value of a pointer to a char.
+ *@s: adress of to
+ *@to: adrres of array or pointer to an array
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;/* change the start addres of the variable s*/
+	/* in the end array start in one addres*/
+}
