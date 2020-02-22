f11dadfdef7edfb390a6013cad907ecc97717ba4
hector lopez <hectorvmlopez@gmail.com>
2020-02-22 15:50:18 +0000

    hector

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -11,11 +11,11 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 	{
-		return (1);
+		return (1); /* complete*/
 	}
 
 	if (*s1 == *s2)
-	{
+	{/* easy case*/
 		return (wildcmp(s1 + 1, s2 + 1));
 
 	}
@@ -24,10 +24,10 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '*')
 	{
 		if (*(s2 + 1) == '*')
-		{
+		{/* easy case*/0
 			return (wildcmp(s1, s2 + 1));
 		}
-
+		/* if statement ......*/
 		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
 		{
 			return (1);
