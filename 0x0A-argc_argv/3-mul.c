398246a5c2886dae5bd853a44bf007f2a838cf18
hector lopez <hectorvmlopez@gmail.com>
2020-02-22 20:49:17 +0000

    hector

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,19 +9,20 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b;
+	int a, b, result;
 
 	a = 0;
 	b = 0;
+	result = 0;
 
 	if (argc > 0)
 	{
 		a = *(*(argv + 1)) - 48;
 		b = *(*(argv + 2)) - 48;
-
+		result = a * b;
 		printf("%d\n", a * b);
 	}
 
-	return (0);
+	return (result);
 }
 
