81df61bc24941d571b3f5375dd8b322a9ee0a877
hector lopez <hectorvmlopez@gmail.com>
2020-02-29 17:18:10 +0000

    hecto

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+int _atoi(char *s);
 /**
  *main - print the multiplication of two numbers
  *@argc: is the numbers of counters
@@ -17,8 +19,8 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		a = *(*(argv + 1)) - 48;
-		b = *(*(argv + 2)) - 48;
+		a = _atoi(*(argv + 1));
+		b = _atoi(*(argv + 2));
 		result = a * b;
 		printf("%d\n", result);
 		return (0);
@@ -30,3 +32,46 @@ int main(int argc, char *argv[])
 
 }
 
+/**
+ *_atoi - convert string to a number
+ *@s: string to convert to an interger
+ *Return: return a string
+ */
+
+int _atoi(char *s)
+{
+	int n, contador;
+	int str, y, i;
+
+	str = 0;
+	y = 0;
+	i = 0;
+	n = 1;
+	contador = 0;
+
+	for (i = 0 ; s[i] != '\0'; i++)
+	{
+		if ((contador > 0) && (s[i] == ' '))
+
+		{
+			str = str;
+			break;
+		}
+
+		if (s[i] == '-')
+		{
+			n = n * -1;
+		}
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			contador++;
+			y = s[i] - '0';
+
+			str = (str * 10) + y;
+		}
+	}
+
+
+	return (str * n);
+}
