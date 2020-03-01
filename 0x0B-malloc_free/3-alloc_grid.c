22abced468aaa055aa384a49ab0eecf3e56e4093
hector lopez <hectorvmlopez@gmail.com>
2020-03-01 22:40:47 +0000

    hector

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *alloc_grid - a n x m matrix
+ *@width: Columns....
+ *@height: Rows.....
+ *Return: matrix.
+ */
+
+
+int **alloc_grid(int width, int height)
+{
+	if ((width == 0 || height == 0) || (width < 0 || height < 0))
+	{
+		return (NULL);
+
+	}
+
+	/*	int j, rows, colum;*/
+	int j;
+	int **Matrix;
+	/* height 1d array*/
+	Matrix = (int **) malloc(sizeof(int *) * height);
+
+	if (Matrix == NULL)
+	{
+		return (NULL);
+	}
+	/* fill array with something*/
+	for (j = 0; j < height; j++)
+	{
+		Matrix[j] = (int *) malloc(sizeof(int) * width);
+	}
+	/*	for (rows = 0; rows < height; rows++)*/
+	/*{*/
+	/*for (colum = 0; colum < height; colum++)*/
+	/*{*/
+	/* Matrix[rows][colum] = 0;*/
+
+	/* }*/
+
+	/*}*/
+
+
+	return (Matrix);
+
+
+}
+
