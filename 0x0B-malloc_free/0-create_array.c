97034ba7597ddb44ab6c35d7e72ea5fb02501cf3
hector lopez <hectorvmlopez@gmail.com>
2020-03-01 19:34:00 +0000

    hector

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,14 +14,14 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	{
-		return (0);
+		return (NULL);
 	}
 
 	char *C = (char *) malloc(size);
 
-	if (C == 0)
+	if (C == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
