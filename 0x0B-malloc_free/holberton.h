d2dd6cb6e8ad0897ac5bf67e9b8831fb6c8181c8
hector lopez <hectorvmlopez@gmail.com>
2020-03-01 19:12:44 +0000

    hector

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,3 +1,5 @@
+#ifndef Holberton_h
+#define Holberton_h
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
@@ -6,3 +8,4 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 int _putchar(char c);
+#endif
