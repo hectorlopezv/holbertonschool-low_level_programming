3a8d6d63bf732bc86a9d9e41a4407a7b5aa33027
hector lopez <hectorvmlopez@gmail.com>
2020-03-01 20:23:43 +0000

    hector

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,21 +8,23 @@
  *Return: return string
  */
 
+
 char *_strdup(char *str)
 {
 	int i, j;
 	char *C;
 
-	if (str == NULL)
+	if (!str)
 	{
 		return (NULL);
 	}
 
+
 	for (j = 0; str[j] != '\0'; j++)
 	{
 
+
 	}
-	j++;
 
 	C = (char *) malloc(j);
 
@@ -30,7 +32,7 @@ char *_strdup(char *str)
 	{
 		C[i] = str[i];
 	}
-	C[i] = str[i];
 
+	C[i] = str[i];
 	return (C);
 }
