2ab58d43307deb75ee4fa85d50f31205542a7ced
hector lopez <hectorvmlopez@gmail.com>
2020-03-01 16:25:15 +0000

    1 exercise

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *create_array - create malloc array and fill it with C char
+ *@size: size of malloc
+ *@c: char with is goign to fill the 1d array
+ *Return: NULL or a pointer to the 1d array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	char *C = (char *) malloc(size);
+
+	if (C == NULL)
+	{
+		return (C);
+	}
+	for (int i = 0; i < size; i++)
+	{
+		C[i] = c;
+	}
+
+	return (C);
+
+}
