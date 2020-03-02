daad90beafa3b58090acab8d605671e6f5fb471a
hector lopez <hectorvmlopez@gmail.com>
2020-03-02 11:59:17 +0000

    hector

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -2,74 +2,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int _strlen(char *s);
+
+
 /**
- *argstostr - use malloc to concatenate string of variable legnth
+ *argstostr - use malloc to concatenate
  *@ac: numbers of arguments
  *@av : array of arguments
- *Return: pointer containing all concatenated strings with a new line
+ *Return: pointer containing all
  */
 
 char *argstostr(int ac, char **av)
 {
-        if (ac == 0 || av == NULL)
-        {
-                return (NULL);
-        }
-        int  sum_char, count, count1, counter, numeros;
-        char *C;
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	int  sum_char, counter, numeros, j;
+	char *C;
 
-        sum_char = 0;
-        counter = 0;
-        numeros = 0;
-	count1 = 0;
+	sum_char = 0;
+	counter = 0;
+	numeros = 0;
+
+	while (counter < ac)
+	{
+		sum_char += _strlen(*av) + 1;
+		av++;
+		counter++;
+	}
+	av -= counter;
+	sum_char = sum_char + 1;
+
+	C = (char *) malloc(sizeof(char) * sum_char);
+
+	if (C == NULL)
+	{
+		return (NULL);
+	}
+
+	counter = 0;
+
+	while (counter < ac)
+	{
+		for (j = 0; *(*(av + counter) + j) != '\0'; j++)
+		{
+			C[numeros] = *(*(av + counter) + j);
+			numeros++;
+
+
+		}
+
+		C[numeros] = '\n';
+		counter++;
+		numeros++;
+
+	}
+	C[numeros] = '\0';
+
+	return (C);
+}
+
+/**
+ *_strlen - legnth of a string
+ *@s: string to be evaluated
+ *Return: return the legnth without
+ */
+
+int _strlen(char *s)
+{
+
+	int count;
+
 	count = 0;
 
-        /*while (counter < ac)*/
-        /*{*/
-             /*   sum_char += _strlen(*av) + 1;*/
-                /*av++;*/
-                /*counter++;*/
-        /*}*/
-        /*sum_char = sum_char + 1;*/
-        
-        /*printf("%d",sum_char);*/
-        
-        for (count = 0; count < ac; count++)
+
+	while (*s != '\0')
 	{
-		for (count1 = 0; av[count][count1] != '\0'; count1++)
-		{
-			sum_char += 1;
-		}
-		sum_char += 1;
+		s++;
+		count++;
 	}
-	sum_char += 1;
-        
-        C = malloc(sum_char);
-        
-        if (C == NULL)
-        {
-                free(C);
-                return (NULL);
-        }
-
-        counter = 0;
-
-        while (counter < ac)
-        {
-                for (int j = 0; *(*(av + counter) + j) != '\0'; j++)
-                {
-                        C[numeros] = *(*(av + counter) + j);
-                        numeros++;
-          
-
-                }
-                
-                C[numeros] = '\n';
-                counter++;
-                numeros++;
-
-        }
-         C[numeros] = '\0';
-
-        return (C);
+	return (count);
 }
