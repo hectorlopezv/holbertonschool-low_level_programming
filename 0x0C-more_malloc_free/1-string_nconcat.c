51c127c30adf137263881b161c5a79c7d415b81a
hector lopez <hectorvmlopez@gmail.com>
2020-03-03 00:39:52 +0000

    hector

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,78 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * _strlen - legnth of a string
+ * @s: a string to be evaluated
+ * Return: return the legnth of a string
+ */
+
+int _strlen(char *s)
+{
+
+	int count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		s++;/* moving the pointer one index*/
+		count++;
+	}
+	return (count);
+}
+
+
+/**
+ * string_nconcat - concatenate a string with n bytes
+ *@s1: string 1
+ *@s2: string 2
+ *@n: number of bytes to be copied
+ *Return: return a pointer to the copied string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	unsigned int size_s1, size_s2;
+	unsigned int k, counter;
+	char *C;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	size_s1 = _strlen(s1);
+	size_s2 = _strlen(s2);
+	if (n > size_s2)
+	{
+		n = size_s2;
+	}
+
+	C = (char *) malloc(size_s1 + size_s2 + 1);
+
+	if (C == NULL)
+	{
+		return (NULL);
+	}
+
+	for (k = 0, counter = 0; k < size_s1; k++, counter++)
+	{
+		C[k] = s1[counter];
+	}
+
+	for (counter = 0; counter < n; counter++, k++)
+	{
+		C[k] = s2[counter];
+	}
+	C[k] = s2[counter];
+
+	return (C);
+}
+
