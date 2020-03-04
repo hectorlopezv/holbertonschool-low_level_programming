12ccc8e6ca366194068f4a68f2a04898f18fc2c8
hector lopez <hectorvmlopez@gmail.com>
2020-03-04 06:47:16 +0000

    hector

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog 
+
+struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
