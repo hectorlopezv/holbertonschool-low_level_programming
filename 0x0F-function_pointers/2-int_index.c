4fc55d66f04cb733a1390fb08bfc797b41cd16a3
hector lopez <hectorvmlopez@gmail.com>
2020-03-04 21:34:01 +0000

    3 exercise

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,10 +13,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-	{
-		return (-1);
-	}
 	if (array == NULL)
 	{
 		return (0);
@@ -24,6 +20,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (cmp == NULL)
 	{
 		return (0);
+	}
+
+	if (size <= 0)
+	{
+		return (-1);
 	}
 
 	for (i = 0; i < size; i++)
