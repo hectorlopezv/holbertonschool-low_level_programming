6ce370d2a4a11e058603a1252d72295852061f27
hector lopez <hectorvmlopez@gmail.com>
2020-03-04 11:25:47 +0000

    4 fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *_strcpy(char *dest, char *src);
+
 /**
  * new_dog - creates a dog struct
  *@name: name
@@ -10,8 +12,10 @@
  *Return: return a struct
  */
 
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, j;
 
 	dog_t *std = malloc(sizeof(dog_t));
 
@@ -19,10 +23,55 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
+	/* we have a reference but we want a copyyyyy*/
+	for (i = 0; name[i] != '\0'; i++)
+	{
+	}
+	i++;
+	for (j = 0; owner[j] != '\0'; j++)
+	{
+	}
+	j++;
+	std->name = malloc(i);
+	if (std->name == NULL)
+	{
+		return (NULL);
+	}
+	_strcpy(std->name, name);
 
-	std->name = name;
+	std->owner = malloc(j);
+	if (std->owner == NULL)
+	{
+		return (NULL);
+	}
+	_strcpy(std->owner, owner);
+
+	/* age is not a reference so is ok we have a copy*/
+	/* we do not affect the original value*/
 	std->age = age;
-	std->owner = owner;
-
 	return (std);
+}
+
+/**
+ * _strcpy - check the code for Holberton School students.
+ *@dest: destination
+ *@src: source
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+	int contador;
+
+	contador = 0;
+
+	while (*src != '\0')
+	{
+		dest[contador] = *src;
+		src++;
+		contador++;
+	}
+	dest[contador] = '\0';
+	return (dest);
 }
