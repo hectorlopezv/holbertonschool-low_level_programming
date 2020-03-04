f3c84f38b2990a7b5362a6230e3691c52f3231ed
hector lopez <hectorvmlopez@gmail.com>
2020-03-04 07:08:23 +0000

    hector

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,14 @@
 #include "dog.h"
 
+/**
+ * init_dog - initiate a dog structure
+ *@d: dog structure
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ *Return: a struct
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
