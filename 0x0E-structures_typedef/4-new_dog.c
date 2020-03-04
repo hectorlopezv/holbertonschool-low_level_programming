9aa4a1032cb274626d3878dbfa8de61755d540c4
hector lopez <hectorvmlopez@gmail.com>
2020-03-04 10:01:09 +0000

    hector

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * dog_t - creates a dog struct
+ *@name: name
+ *@age:
+ *@owner:
+ *Return: return a struct
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	struct dog *std;
+
+	std -> name = name;
+	std -> age = age;
+	std -> owner = owner;
+	return (std);
+}
