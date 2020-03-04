6e0f91448e19ce92a22250a7aee9a5966b8dafc9
hector lopez <hectorvmlopez@gmail.com>
2020-03-04 10:32:31 +0000

    hector

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * new_dog - creates a dog struct
@@ -11,10 +12,13 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t st;
-	dog_t *std;
 
-	std = &st;
+	dog_t *std = malloc(sizeof *std);
+
+	if (std == NULL)
+	{
+		return (NULL);
+	}
 
 	std->name = name;
 	std->age = age;
