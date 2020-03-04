6f8bfe49dd6242c5c927058125437c2ec67a7eb1
hector lopez <hectorvmlopez@gmail.com>
2020-03-04 12:03:04 +0000

    hector

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog dog_t;
 
 /**
  *struct dog - struct containing name,age,owner of a dog
@@ -16,8 +15,10 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
 #endif
