fa5d6c96514538a0305f1aa4cf13205194817b47
hector lopez <hectorvmlopez@gmail.com>
2020-03-04 11:36:15 +0000

    4 fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,7 +18,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int i, j;
 
 	dog_t *std = malloc(sizeof(dog_t));
-
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
 	if (std == NULL)
 	{
 		return (NULL);
