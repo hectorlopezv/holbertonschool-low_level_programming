d5625f1ffc1d99cd923983302371fdfafee30fd6
hector lopez <hectorvmlopez@gmail.com>
2020-03-04 07:33:00 +0000

    hector

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,27 +1,27 @@
 #include "dog.h"
 #include <stdio.h>
 
+
 void print_dog(struct dog *d)
 {
-	if ( d == NULL)
+	if (d == NULL)
 	{
 		return;
 	}
-	if (d ->name == NULL)
+	if (d->name == NULL)
 	{
 		printf("(nill)\n");
 	}
-	if (d ->age == 0.0)
-	{
-		printf("(nill)\n");
 
-	}
-	if (d ->owner == NULL)
+	if (d->age == 0.0)
 	{
 		printf("(nill)\n");
 
 	}
+	if (d->owner == NULL)
+	{
+		printf("(nill)\n");
 
-
+	}
 
 }
