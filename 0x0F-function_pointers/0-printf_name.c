e6c3e33f7b9cd94dfc201094f2a4420144a1bc91
hector lopez <hectorvmlopez@gmail.com>
2020-03-04 20:55:32 +0000

    exercise 0

---
+++ 0x0F-function_pointers/0-printf_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name -  print the name depending of the functions
+ *@name: string of the name
+ *@f: generic function
+ *Return: return nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+
+}
+
