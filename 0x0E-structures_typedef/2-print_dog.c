d2defa80021eb4414e43e4dcfd786df6804d7d1d
hector lopez <hectorvmlopez@gmail.com>
2020-03-04 07:36:15 +0000

    fix for 2 exercise

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,24 +4,35 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-	{
-		return;
-	}
-	if (d->name == NULL)
-	{
-		printf("(nill)\n");
-	}
 
-	if (d->age == 0.0)
+	if(d->name == NULL)
 	{
-		printf("(nill)\n");
+		printf("Name: (nill)");
+	}
+	else
+	{
+		printf("Name: %s\n",d->name);
 
 	}
-	if (d->owner == NULL)
-	{
-		printf("(nill)\n");
 
+	if(d->age == 0.0)
+	{
+		printf("Age: (nill)");
+
+	}
+	else
+	{
+		printf("Age: %f\n",d->age);
+	}
+
+	if(d->owner == NULL )
+	{
+		printf("Owner: (nill)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n",d->owner);
 	}
 
 }
+
