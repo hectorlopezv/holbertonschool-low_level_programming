9238fef79db4b494cae8fa77c8c43715d2d21a88
hector lopez <hectorvmlopez@gmail.com>
2020-03-04 21:31:52 +0000

    3 exercise

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,38 @@
+#include "function_pointers.h"
+
+
+/**
+ *int_index -  int_index
+ *@array: array of intergers
+ *@size: size of the array
+ *@cmp: function_pointers
+ *Return: return the index where the condition is met
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (array == NULL)
+	{
+		return (0);
+	}
+	if (cmp == NULL)
+	{
+		return (0);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
