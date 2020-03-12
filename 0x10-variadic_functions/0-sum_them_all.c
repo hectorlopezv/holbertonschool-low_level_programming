29ec2f35827a56c58945af542f2a798274961dfb
hector lopez <hectorvmlopez@gmail.com>
2020-03-12 17:00:16 +0000

    0 exercise

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,14 +15,14 @@ int sum_them_all(const unsigned int n, ...)
 
 	sum = 0;
 
-	if (n == 0 )
+	if (n == 0)
 	{
 		return (0);
 	}
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(args,int);
+		sum += va_arg(args, int);
 
 	}
 
