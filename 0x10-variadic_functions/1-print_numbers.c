bde712321e6e26bf7f934f4df90d1030be559657
hector lopez <hectorvmlopez@gmail.com>
2020-03-12 17:32:42 +0000

    1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,43 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *print_numbers - print_numbers
+ *@separator: the separator
+ *@n: number of const
+ *Return: return the string separated
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n);
+
+
+
+
+	if (separator == NULL)
+	{
+		return;
+	}
+	if (n <= 0)
+	{
+		return;
+	}
+
+	printf("%d", va_arg(args, int));
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%s%d", separator, va_arg(args, int));
+
+	}
+	printf("\n");
+
+	va_end(args);
+
+}
