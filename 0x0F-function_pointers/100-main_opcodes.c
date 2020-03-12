04ce4820f5f39f938bb6b6ddae697d0626402eb3
hector lopez <hectorvmlopez@gmail.com>
2020-03-12 04:52:53 +0000

    last advanced

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int number_bytes, i;
+	unsigned char *start = (unsigned char *)&main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	number_bytes = atoi(argv[1]);
+	if (number_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	/*printf("0x%08x\n",f);*/
+
+	for (i = 0; i < number_bytes; i++)
+	{
+		printf("%x\n", start[i]);
+	}
+
+}
