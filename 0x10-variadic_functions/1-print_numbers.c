6bad09aa073fd3f5fd4030e622f21fdd902318fe
hector lopez <hectorvmlopez@gmail.com>
2020-03-12 17:48:25 +0000

    1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,6 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (n <= 0)
 	{
+		printf("\n");
 		return;
 	}
 
@@ -28,18 +29,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		separator = "";
 	}
 
-	for (i = 0; i < n; i++)
+	printf("%d", va_arg(args, int));
+
+	for (i = 0; i < n - 1; i++)
 	{
-		if (i == 0)
-		{
-			printf("%d", va_arg(args, int));
+		printf("%s%d", separator, va_arg(args, int));
 
-		}
-		else
-		{
-			printf("%s%d", separator, va_arg(args, int));
-
-		}
 	}
 	printf("\n");
 
