dd3efc623fa2a9a347688d2e09ffa72722a33cb9
hector lopez <hectorvmlopez@gmail.com>
2020-03-13 00:05:47 +0000

    3 exercise

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -13,4 +13,24 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 
+void (* get_op(char *format) )(char *, va_list, int, int);
+
+struct cases
+{
+	    char *format;
+		    void (*f) (char * separator,va_list temp,int size,int contador_space);
+};
+typedef struct cases switc_h;
+
+
+void print_c(char * separator,va_list temp,int size,int contador_space);
+int _str_len(const char * const format);
+
+void print_i(char * separator,va_list temp,int size,int contador_space);
+
+void print_f(char * separator,va_list temp,int size,int contador_space);
+
+void print_s(char * separator,va_list temp,int size,int contador_space);
+
+
 #endif
