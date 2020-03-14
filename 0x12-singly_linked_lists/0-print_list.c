a54da2715288ee988bd75fc5a0fbe4a6285e621d
hector lopez <hectorvmlopez@gmail.com>
2020-03-14 23:59:59 +0000

    1 first exercise

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ *print_list -  print the content of a node
+ *@h: node to evaluate
+ *Return: return the printed node
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *head;
+	int i;
+
+	i = 0;
+	head = h;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (head != NULL)
+	{
+
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		head = head->next;
+		i++;
+	}
+
+	return (i);
+}
