38b5156da715a77ffcbde22038a3a69b5ed4724b
hector lopez <hectorvmlopez@gmail.com>
2020-03-15 05:52:00 +0000

    0 exercise

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ *print_listint -  print the elements of a list
+ *@h: linked list.
+ *Return: the numbers of elements found
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i;
+	const listint_t *temp;
+
+	temp = h;
+	i = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (temp!= NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		i++;
+
+	}
+
+	return (i);
+}
