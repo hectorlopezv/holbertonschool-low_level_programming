b6a3cd318af3f89b4bfd0a793b54c54492bc789e
hector lopez <hectorvmlopez@gmail.com>
2020-03-24 06:53:28 +0000

    fix for exercises

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,10 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	if (*head == NULL)
-	{
-		return (NULL);
-	}
+
 	temp_it = *head;
 
 	temp = malloc(sizeof(listint_t));
@@ -41,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp_it = temp_it->next;
 	}
-	if (*head == temp || temp_it != NULL)
+	if (temp_it != NULL)
 	{
 		temp->next = temp_it->next;
 		temp_it->next = temp;
