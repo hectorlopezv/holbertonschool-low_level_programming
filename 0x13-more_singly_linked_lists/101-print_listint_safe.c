25886682c2b1878880ed50471783e866bfa8757a
hector lopez <hectorvmlopez@gmail.com>
2020-03-24 11:02:57 +0000

    print for circular link list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ *print_listint_safe -  print the elements of a list
+ *@head: linked list.
+ *Return: the numbers of elements found
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	int i;
+	const listint_t *temp;
+
+	temp = head;
+	i = 0;
+
+	if (head == NULL)
+	{
+		exit(98);
+	}
+	while (temp != NULL)
+	{
+		printf("[%p] %d\n", (void *)temp, temp->n);
+
+		if (temp <= temp->next)
+		{
+			break;
+		}
+		else
+		{
+			temp = temp->next;
+			i++;
+		}
+	}
+
+	return (i);
+}
