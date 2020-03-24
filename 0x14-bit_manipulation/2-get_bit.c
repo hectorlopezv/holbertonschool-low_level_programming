45c28dbd4bc0cca974f7bb880d3b75a13e81b324
hector lopez <hectorvmlopez@gmail.com>
2020-03-24 16:56:45 +0000

    hh

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *get_bit - returns the value of a bit at a given index.
+ *@n: numbers to convert
+ *@index: index of the numbers
+ *Return: The value of the bit at index or -1 if an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 64)
+	{
+		return (-1);
+	}
+
+	return ((n >> index) & 1);
+}
+
