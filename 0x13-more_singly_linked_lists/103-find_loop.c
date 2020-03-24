4d9f422f1c9023fb29cc22162ff57dc1ed3d48af
hector lopez <hectorvmlopez@gmail.com>
2020-03-24 14:26:50 +0000

    #14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,73 +11,32 @@ listint_t *get_start_of_node(listint_t *head, listint_t *node);
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *node_where_they_meet, *start_loop_node;
+	listint_t *fast, *slow;
 
+	slow = fast = head;
 	if (head == NULL)
 	{
 		return (NULL);
 	}
-
-	node_where_they_meet = checkforloop(head);
-
-	if (node_where_they_meet == NULL)
-	{
-		return (NULL);
-	}
-	start_loop_node = get_start_of_node(head, node_where_they_meet);
-
-	return (start_loop_node);
-
-
-
-}
-
-
-/**
- *checkforloop - checkforloop
- *@head: head of the list
- *Return: return the if the is loop or not
- */
-
-listint_t *checkforloop(listint_t *head)
-{
-	listint_t *slow, *fast;
-
-	slow = head, fast = head;
-
 	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 		if (slow == fast)
 		{
-			return (slow);
+			break;
 		}
-
 	}
-	return (NULL);
-
-}
-
-
-/**
- *get_start_of_node - get start of loop
- *@head: head of the list
- *@node: node where they meet
- *Return: return the place where they meet
- */
-
-listint_t *get_start_of_node(listint_t *head, listint_t *node)
-{
-	listint_t *slow;
-
+	if (slow == NULL || fast == NULL || fast->next == NULL)
+	{
+		return (NULL);
+	}
 	slow = head;
-
-	while (slow != node)
+	while (slow != fast)
 	{
 		slow = slow->next;
-		node = node->next;
+		fast = fast->next;
 	}
 	return (slow);
-}
 
+}
