1047cab4d3ec715ecde1ea6e5640567ca50e4716
hector lopez <hectorvmlopez@gmail.com>
2020-03-24 06:49:10 +0000

    fix for exercises

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
 	temp_it = *head;
 
 	temp = malloc(sizeof(listint_t));
