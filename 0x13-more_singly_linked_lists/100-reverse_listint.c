bf2fc43f4942590cd60c6ca815011dcff893fa7b
hector lopez <hectorvmlopez@gmail.com>
2020-03-24 03:30:03 +0000

    #reverse list iterative way

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *previous, *next;
+
+	previous = NULL;
+
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+
+	while(*head != NULL)
+	{
+		next =  (*head)->next;
+		(*head)->next = previous;
+		previous = *head;
+		*head = next;
+	}
+
+	*head = previous;
+	return (previous);
+
+}
+
