c2f03bcc585c4d148ae05a521a821fce15346694
hector lopez <hectorvmlopez@gmail.com>
2020-03-24 16:58:12 +0000

    hh

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,20 +1,19 @@
 #include "holberton.h"
-#include "unistd.h"
 
 /**
- *print_binary - bynary to int
- *@n: string
- *Return: unsigned int
+ *print_binary - prints the binary representation of a number
+ *@n: lon int for the binary numbers
  */
 
 void print_binary(unsigned long int n)
 {
-	if (n == 1)
+	if (n == 0)
 	{
-		_putchar('1');
+		_putchar('0');
 		return;
 	}
-	else if (n == 0)
+
+	if (n == 1)
 	{
 		_putchar('1');
 		return;
@@ -23,9 +22,4 @@ void print_binary(unsigned long int n)
 	print_binary(n >> 1);
 
 	_putchar('0' + (n & 1));
-
-
-
-
 }
-
