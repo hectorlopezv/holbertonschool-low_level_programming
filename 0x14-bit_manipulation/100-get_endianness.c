04506ce5045d35ceb8b09a69720bcfa167000d6d
hector lopez <hectorvmlopez@gmail.com>
2020-03-24 17:06:18 +0000

    hh

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -10,9 +10,8 @@ int get_endianness(void)
 	unsigned int i = 1;
 	char *c = (char*)&i;
 	if (*c)
-		printf("Little endian");
-	else
-		printf("Big endian");
-	getchar();
+		return (1);
+
+
 	return 0;
 }
