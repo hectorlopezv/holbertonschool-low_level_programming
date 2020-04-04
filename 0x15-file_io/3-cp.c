7a204a3671cb3a9ac26a8a32f105f41d37e2225e
hector lopez <hectorvmlopez@gmail.com>
2020-04-04 18:58:43 +0000

    #3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,44 +17,36 @@ int main(int argc, char **argv)
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		exit(97); }
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		exit(98); }
 	fto = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, flag);
 	if (fto == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		exit(99); }
 	do {/*read and write at least one time*/
 		n_bytes = read(from, c, 1024);
 		if (n_bytes < 0) /* there is no end line and no error*/
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
-			exit(98);
-		}
-
+			exit(98); }
 		file = write(fto, c, n_bytes);
 		if (file == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			exit(99); }
 	} while (n_bytes == 1024);
 	if (close(from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
-		exit(100);
-	}
+		exit(100); }
 	if (close(fto) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fto);
-		exit(100);
-	}
+		exit(100); }
 	return (0);
 }
