d9bc5d6a0edd022641d17449d85f39e435af307f
hector lopez <hectorvmlopez@gmail.com>
2020-04-04 17:39:21 +0000

    #2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1 || (text_content == NULL && fd == -1))
+	if (text_content == NULL && fd == -1)
+	{
+		return (-1);
+	}
+	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
 	{
