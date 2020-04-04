5b586ec1b603433a4e67d0b9afb57f0b9c26a029
hector lopez <hectorvmlopez@gmail.com>
2020-04-04 16:47:41 +0000

    #1 exercise

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,19 @@
 #include "holberton.h"
 
+/**
+ *create_file - create_file
+ *@filename: filename
+ *@text_content: text_content
+ *Return: return 1 on success and 0 on failure
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd, ret, i;
 
 	if (filename == NULL)
 		return (0);
-	fd = open (filename, O_WRONLY | O_TRUNC | O_CREAT , S_IWUSR | S_IRUSR);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IWUSR | S_IRUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
@@ -19,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	{
 	};
 
-	ret = write (fd, text_content, i);
+	ret = write(fd, text_content, i);
 	close(fd);
 	if (ret == -1)
 		return (-1);
