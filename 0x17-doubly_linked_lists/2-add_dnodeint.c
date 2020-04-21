7d3591fdccc06973d8d1ebbcad3bf354673075a8
hector lopez <hectorvmlopez@gmail.com>
2020-04-21 23:40:10 +0000

    hector

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,8 +11,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (head == NULL)
 		return (NULL);
-	if (*head == NULL)
-		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -20,7 +18,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
-
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 	(*head)->prev = new_node;
 	new_node->next = *head;
 	*head = new_node;
