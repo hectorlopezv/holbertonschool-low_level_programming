8203d18266f594d0a24c086711cc64659f3c051e
hector lopez <hectorvmlopez@gmail.com>
2020-04-22 01:11:07 +0000

    4 exercise

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_dlistint - free list
+ *@head: head of the list
+ *Return: free link list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	temp = head;
+	if (head == NULL)
+		return;
+
+	while (head && head->next)
+	{
+		head = head->next;
+		free(temp);
+		temp = head;
+	}
+	head = NULL;
+}
