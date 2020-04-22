1d68983f6ca7017de5d866d1bcc57dd36c2ef35d
hector lopez <hectorvmlopez@gmail.com>
2020-04-22 00:05:27 +0000

    fix 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node, *temp_node, *prev_node;
+	dlistint_t *new_node, *temp_node;
 
 	if (head == NULL)
 		return (NULL);
@@ -13,12 +13,15 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->next = NULL;
 	new_node->prev = NULL;
 	if (*head == NULL)
+	{
 		*head = new_node;
-	while (temp_node)
-	{	prev_node = temp_node;
+		return (new_node);
+	}
+	while (temp_node && temp_node->next)
+	{
 		temp_node = temp_node->next;
 	}
-	prev_node->next = new_node;
-	new_node->prev = prev_node;
+	temp_node->next = new_node;
+	new_node->prev = temp_node;
 	return (new_node);
 }
