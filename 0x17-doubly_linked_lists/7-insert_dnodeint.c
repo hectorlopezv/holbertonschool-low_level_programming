b44d3c063f011106cc3d65b2681b2cd5e5036ffd
hector lopez <hectorvmlopez@gmail.com>
2020-04-22 02:03:12 +0000

    fix for number 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,6 +23,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
+	if (idx == 0)
+		return (add_dnodeint(h,n));
 	while (temp)
 	{
 		if (i == idx - 1)
